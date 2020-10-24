546e764a187efe3df7c8bda7ffa3b5e8a035acb3
johnrey-code <73368810+johnrey-code@users.noreply.github.com>
2020-10-24 14:32:20 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+const form = document.getElementById('form')
+const form = document.getElementById('firstname')
+const form = document.getElementById('lastname')
+const form = document.getElementById('email')
+
+    form.addEventListener('submit', (e) => {e.preventDefault(); checkinputs});
+
+    function checkinputs () {
+        const 
+    }
